3c85d9fe005bc6a0dd84dc82f10b949b814de0f1
areeba978621 <areeba.35731@stu.upes.ac.in>
2026-09-11 21:01:12 +0530

    Add files via upload

---
+++ day4_code2.c
@@ -0,0 +1,10 @@
+//Q8: Write a program to find and display the sum of the first n natural numbers.
+#include <stdio.h>
+int main() {
+    int n, sum;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    sum = n * (n + 1) / 2;
+    printf("Sum=%d\n", sum);
+    return 0;
+}
