8c134047507965c4b723213d03e83444d818d06e
areeba978621 <areeba.35731@stu.upes.ac.in>
2026-09-21 23:14:47 +0530

    Add files via upload

---
+++ day15_code1.c
@@ -0,0 +1,12 @@
+//Q29: Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main() {
+    int n, factorial = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        factorial = factorial * i;
+    }
+    printf("Factorial of %d: %d\n", n, factorial);
+    return 0;
+}
