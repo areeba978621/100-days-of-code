72b6daff36b953d7dece4fd22ddf2e7c66bbe49e
areeba978621 <areeba.35731@stu.upes.ac.in>
2026-09-24 22:17:20 +0530

    Add files via upload

---
+++ day23_code2.c
@@ -0,0 +1,22 @@
+//Q46: Write a program to print the following pattern:
+/*
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include <stdio.h>
+int main() {
+    int i, j;
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
